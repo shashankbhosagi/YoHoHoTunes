baed1761956dd212388933b5a313b3b9e9de1384
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-08-22 22:10:49 +0530

    🎉 init

---
+++ README.md
@@ -0,0 +1,34 @@
+# YoHoHoTunes: YouTube to MP3 Converter
+
+**YoHoHoTunes** is a JavaScript-based tool that transforms YouTube links into MP3 files. This project utilizes the power of Rapid API to seamlessly convert video links into downloadable audio tracks.
+
+## How It Works
+
+1. **Input**: Provide a YouTube id of the desired video.
+2. **Processing**: The Rapid API processes the link and extracts the audio content.
+3. **Conversion**: The tool converts the audio content into high-quality MP3 format.
+4. **Download**: Receive the MP3 file, ready for your musical enjoyment!
+
+## Features
+
+- Easy and intuitive YouTube to MP3 conversion.
+- Rapid API integration for seamless link processing.
+- High-quality MP3 output for an enhanced listening experience.
+
+## Usage
+
+1. Enter a valid YouTube id in the provided input field.
+2. Click the "Convert" button.
+3. Once processed, click the "Download" button to save your MP3 file.
+
+## Credits
+
+- **Rapid API**: For their excellent API service that powers the link conversion.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+**YoHoHoTunes** - Let the melodies of the digital sea enrich your ears! Yohohoho!
---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+//create express server
+const app = express();
+
+//port of server
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/", async (req, res) => {
+  const videoId = req.body.videoID;
+  // console.log(videoId);
+  if (videoId === undefined || videoId === null || videoId === "") {
+    return res.render("index", {
+      success: false,
+      message: "Yohohoho! Oops, wrong YouTube ID, matey. Find the real one for music to flow, aye?",
+    });
+  } else {
+    const fetchAPI = await fetch(
+      `https://youtube-mp3-download1.p.rapidapi.com/dl?id=${videoId}`,
+      {
+        "method": "GET",
+        "headers":{
+        "X-RapidAPI-Key": process.env.API_KEY,
+        "X-RapidAPI-Host": process.env.API_HOST,
+        }
+      }
+    );
+    const fetchResponse = await fetchAPI.json();
+    if (fetchResponse.status === "ok") {
+      return res.render("index", {
+        success: true,
+        song_title: fetchResponse.title,
+        song_link: fetchResponse.link,
+        message:"Song retrieved, matey! Enjoy and collect the melody. Yohohoho, cheerio!"
+      });
+    } else {
+      return res.render("index", {
+        success: false,
+        message: "Navigational error, matey! Invalid API request, lost in digital currents. Yohohoho!",
+      });
+    }
+  }
+});
+
+//start the server
+app.listen(PORT, () => {
+  console.log(`Server started On PORT: ${PORT}`);
+});
